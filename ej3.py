ba77220a5f02aaf664a4f0983b2352e11a597167
GitUser2004 <dylan.gutierrez.h@gmail.com>
2025-02-24 21:34:53 +0000

    new

---
+++ ej3.py
@@ -0,0 +1,7 @@
+name=input("give me your name:")
+age=input("give me your age:")
+prof=input("give me your prof:")
+
+print("your name is ",name," your age is ",age," and your proffesion is ",prof)
+
+# suficientemente diferente 
